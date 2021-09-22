4152ba56b23e3542060994f7d5d3c8bd7366091b
Shehzad Hashmi <91077741+shehzadhashmis@users.noreply.github.com>
2021-09-22 14:57:01 +0500

    updated formatting

--- googlecodeformatter/src/main/java/io/hashmis/googlecodeformatter/GoogleCodeFormatterApplication.java
+++ googlecodeformatter/src/main/java/io/hashmis/googlecodeformatter/GoogleCodeFormatterApplication.java
@@ -5,37 +5,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * A Spring Boot application class to demonstrate Java code formatting with eclipse style
+ * A Spring Boot application class to demonstrate Java code formatting with google style
  * formatting.
  *
  * @author shehzadhashmis
  */
 @Slf4j
 @SpringBootApplication
-public class GoogleCodeFormatterApplication {
+public class
+GoogleCodeFormatterApplication 
+{
 
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
 
     SpringApplication.run(GoogleCodeFormatterApplication.class, args);
 
-    methodWithMultipleArguments(
-        "Mr", "John", "Wick", "123456789", "jwick@testformatter.com", "test code formatter");
+    methodWithMultipleArguments("Mr", "John", "Wick", "123456789", "jwick@testformatter.com", "test code formatter");
   }
 
-  private static void methodWithMultipleArguments(
-      String title,
-      String firstName,
-      String lastName,
-      String phoneNumber,
-      String email,
-      String description) {
-    log.info(
-        "Inside GoogleCodeFormatterApplication.callMethodWithMultipleArguments method with title:{}, firstName:{}, lastName:{}, phoneNumber:{}, email:{}, description:{}",
-        title,
-        firstName,
-        lastName,
-        phoneNumber,
-        email,
-        description);
+  private static void methodWithMultipleArguments(String title, String firstName, String lastName, String phoneNumber, String email, String description) 
+  {
+    log.info("Inside GoogleCodeFormatterApplication.callMethodWithMultipleArguments method with title:{}, firstName:{}, lastName:{}, phoneNumber:{}, email:{}, description:{}", title, firstName, lastName, phoneNumber, email, description);
   }
 }
