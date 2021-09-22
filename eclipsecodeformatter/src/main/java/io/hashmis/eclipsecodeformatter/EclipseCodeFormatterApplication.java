d0b70663dade919f1572e763a458cc993e07b4f1
Shehzad Hashmi <91077741+shehzadhashmis@users.noreply.github.com>
2021-09-22 15:54:40 +0500

    updated formatting

--- eclipsecodeformatter/src/main/java/io/hashmis/eclipsecodeformatter/EclipseCodeFormatterApplication.java
+++ eclipsecodeformatter/src/main/java/io/hashmis/eclipsecodeformatter/EclipseCodeFormatterApplication.java
@@ -11,20 +11,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @Slf4j
 @SpringBootApplication
-public class EclipseCodeFormatterApplication {
+public class 
+EclipseCodeFormatterApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         SpringApplication.run(EclipseCodeFormatterApplication.class, args);
 
-        methodWithMultipleArguments("Mr", "John", "Wick", "123456789", "jwick@testformatter.com",
-                "test code formatter");
+        methodWithMultipleArguments("Mr", "John", "Wick", "123456789", "jwick@testformatter.com", "test code formatter");
     }
 
-    private static void methodWithMultipleArguments(String title, String firstName, String lastName, String phoneNumber,
-            String email, String description) {
-        log.info(
-                "Inside EclipseCodeFormatterApplication.callMethodWithMultipleArguments method with title:{}, firstName:{}, lastName:{}, phoneNumber:{}, email:{}, description:{}",
-                title, firstName, lastName, phoneNumber, email, description);
+    private static void methodWithMultipleArguments(String title, String firstName, String lastName, String phoneNumber, String email, String description) 
+    {
+        log.info("Inside EclipseCodeFormatterApplication.callMethodWithMultipleArguments method with title:{}, firstName:{}, lastName:{}, phoneNumber:{}, email:{}, description:{}", title, firstName, lastName, phoneNumber, email, description);
     }
 
 }
